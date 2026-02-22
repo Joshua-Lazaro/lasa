8f9641c14a53843ceb731c958f347be875152be5
YuriUrri <urrijehan1@gmail.com>
2026-02-22 23:53:09 +0800

    Update SearchBar.js

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { useState } from "react";
@@ -12,23 +11,36 @@ export default function SearchBar({ onSearch }) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="w-85 lg:w-full max-w-md mx-auto flex items-center border-2 border-gray-300 rounded-lg overflow-hidden">
-        <input
-            type="text"
-            placeholder="Search recipes..."
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            className="grow p-2 outline-none"
+    <form
+      onSubmit={handleSubmit}
+      className="w-85 lg:w-full max-w-md mx-auto flex items-center border-2 border-gray-300 rounded-lg overflow-hidden"
+    >
+      <input
+        type="text"
+        placeholder="Search recipes..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        className="grow p-2 outline-none"
+      />
+
+      {/* Image Button */}
+      <button
+        type="button"
+        className="w-16 h-10 flex justify-center items-center ml-2 bg-gray-0 rounded-lg hover:scale-110 transition-transform duration-200 cursor-pointer"
+      >
+        <img
+          src="/aiSmartSuggest.png"
+          alt="Filter"
+          className="w-12 h-10 object-contain"
         />
-        <button type="button" className="w-10 ml-2 text-white px-4 py-2 rounded-lg hover:scale-130 transition-transform duration-200 cursor-pointer">
-            <img src="/aiSmartSuggest.png" alt="Filter" className="w-15 h-5 object-contain" />
-        </button>
-        <button
-            type="submit"
-            className="bg-blue-500 text-white  px-4 py-2 rounded-r-lg hover:bg-blue-600 transition-colors"
-        >
-         Search
-        </button>
+      </button>
+
+      <button
+        type="submit"
+        className="bg-blue-500 text-white px-4 py-2 rounded-r-lg hover:bg-blue-600 transition-colors"
+      >
+        Search
+      </button>
     </form>
   );
 }
